552f915321ee93e4939a70135dbc07926053ff74
Vishal22598 <vishalgupta22598@gmail.com>
2025-04-14 14:55:27 +0530

    change1

--- sunbeamblog/src/pages/SearchBlog.jsx
+++ sunbeamblog/src/pages/SearchBlog.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import './allBlog.css'
 function SearchBlog() {
   return (
     <div>
-      <h1>Search Blog</h1>
+      <h1 className='page-header'>Create Blog</h1>
+      <div className="container">
+        <div className="leftbox">
+
+        </div>
+        <div className="rightbox">
+            <div className="table-container">
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody id='tbody'>
+                        <tr>
+                            <td>1</td>
+                            <td>title</td>
+                            <td>cat</td>
+                            <td>
+                                <button>edit</button>
+                                <button>delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+      </div>
     </div>
   )
 }
